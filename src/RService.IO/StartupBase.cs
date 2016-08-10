f252e5f3b4defe34e8ce424dbf1c180f887c619f
James Stumme <jstumme@pillartechnology.com>
2016-08-09 23:50:06 -0500

    Adds routing support.

---
+++ src/RService.IO/StartupBase.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace RService.IO
+{
+    /// <summary>
+    /// Startup base class that will auto-wire route.
+    /// </summary>
+    public abstract class StartupBase
+    {
+        /// <summary>
+        /// Constructs a startup base and scans for routes and services in assemblies.
+        /// </summary>
+        /// <param name="assemblies">Array of <see cref="Assembly"/>s to scan for routes and services.</param>
+        protected StartupBase(params Assembly[] assemblies)
+        {
+            
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public virtual void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public virtual void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+        }
+    }
+}
---
+++ test/RService.IO.Tests/StartupBaseTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace RService.IO.Tests
+{
+    public class StartupBaseTests
+    {
+
+        [Fact]
+        public void ConfigureService__AddsRouting()
+        {
+            var services = new ServiceCollection();
+            var startup = new Startup();
+            
+            startup.ConfigureServices(services);
+
+            Assert.True(services.Any(x => x.ImplementationType?.Name.Equals("RoutingMarkerService") ?? false));
+        }
+
+
+        private class Startup : StartupBase { }
+    }
+}
