63ebeba9b6508067bc69a3f85e858039245702f9
James Stumme <jstumme@pillartechnology.com>
2016-08-13 11:52:40 -0500

    Startup can scan assembly for route attributes.

--- src/RService.IO/StartupBase.cs
+++ src/RService.IO/StartupBase.cs
@@ -1,4 +1,8 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -14,12 +18,43 @@ namespace RService.IO
     public abstract class StartupBase
     {
         /// <summary>
+        /// The <see cref="RouteHandler"/> to process request routing.
+        /// </summary>
+        public Func<HttpContext, Task> RouteHanlder { get; set; } = InternalRouteHandler;
+        public Dictionary<string, RouteAttribute> Routes { get; }
+
+        /// <summary>
         /// Constructs a startup base and scans for routes and services in assemblies.
         /// </summary>
         /// <param name="assemblies">Array of <see cref="Assembly"/>s to scan for routes and services.</param>
         protected StartupBase(params Assembly[] assemblies)
         {
-            
+            Routes = new Dictionary<string, RouteAttribute>();
+
+            // Scan assemblies
+            foreach (var assembly in assemblies)
+            {
+                var classes = assembly.GetTypes().Where(x => x.ImplementsInterface<IService>()).ToList();
+                var methodsWithAttribute = classes.SelectMany(x => x.GetPublicMethods()).Where(x => x.HasAttribute<RouteAttribute>()).ToList();
+                var methodsParamWithAttribute = classes.SelectMany(x => x.GetPublicMethods()).Where(x => x.HasParamWithAttribute<RouteAttribute>()).ToList();
+
+                methodsWithAttribute.ForEach(x =>
+                {
+                    var attrs = x.GetCustomAttributes<RouteAttribute>().ToList();
+                    attrs.ForEach(attr => Routes.Add(GetNameForMethodRoute(x, attr), attr));
+                });
+
+                methodsParamWithAttribute.ForEach(method =>
+                {
+                    var type = method.GetParamWithAttribute<RouteAttribute>();
+                    var attrs = type?.GetAttributes<RouteAttribute>().ToList();
+                    attrs?.ForEach(a =>
+                    {
+                        var attr = (RouteAttribute) a;
+                        Routes.Add(GetNameForParamRoute(type, attr), attr);
+                    });
+                });
+            }
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -32,6 +67,45 @@ namespace RService.IO
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public virtual void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            var trackPackageRouteHandler = new RouteHandler(context =>
+            {
+                var routeValues = context.GetRouteData().Values;
+                return context.Response.WriteAsync(
+                    $"Hello! Route values: {string.Join(", ", routeValues)}");
+            });
+
+            var routeBuilder = new RouteBuilder(app, trackPackageRouteHandler);
+
+            routeBuilder.MapRoute(
+                "Track Package Route",
+                "package/{operation:regex(^track|create|detonate$)}/{id:int}");
+
+            routeBuilder.MapGet("hello/{name}", context =>
+            {
+                var name = context.GetRouteValue("name");
+                // This is the route handler when HTTP GET "hello/<anything>"  matches
+                // To match HTTP GET "hello/<anything>/<anything>, 
+                // use routeBuilder.MapGet("hello/{*name}"
+                return context.Response.WriteAsync($"Hi, {name}!");
+            });
+
+            var routes = routeBuilder.Build();
+            app.UseRouter(routes);
+        }
+
+        protected static string GetNameForMethodRoute(MethodInfo info, RouteAttribute attribute)
+        {
+            return $"{info.DeclaringType.FullName}.{info.Name} {attribute.Verbs} {attribute.GetHashCode()}";
+        }
+
+        protected static string GetNameForParamRoute(Type type, RouteAttribute attribute)
+        {
+            return $"{type.FullName} {attribute.Verbs} {attribute.GetHashCode()}";
+        }
+
+        protected static Task InternalRouteHandler(HttpContext context)
+        {
+            throw new NotImplementedException();
         }
     }
 }
