c87398184752552d31e7de528d53cecc19b4a9a4
James Stumme <jstumme@pillartechnology.com>
2016-08-14 18:26:21 -0500

    Cleanup extra unneeded routes

--- src/RService.IO/StartupBase.cs
+++ src/RService.IO/StartupBase.cs
@@ -74,19 +74,6 @@ namespace RService.IO
                 MapRoute(routeBuilder, route.Value);
             }
 
-            routeBuilder.MapRoute(
-                "Track Package Route",
-                "package/{operation:regex(^track|create|detonate$)}/{id:int}");
-
-            routeBuilder.MapGet("hello/{name}", context =>
-            {
-                var name = context.GetRouteValue("name");
-                // This is the route handler when HTTP GET "hello/<anything>"  matches
-                // To match HTTP GET "hello/<anything>/<anything>, 
-                // use routeBuilder.MapGet("hello/{*name}"
-                return context.Response.WriteAsync($"Hi, {name}!");
-            });
-
             var routes = routeBuilder.Build();
             app.UseRouter(routes);
         }
