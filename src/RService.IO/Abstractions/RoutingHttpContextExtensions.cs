b4a8b1c6b7d19d643c31bde3bd30275ba4aa8d8d
James Stumme <jamesstumme@gmail.com>
2016-08-23 23:06:44 -0500

    Added extension method to get route handler from context if set by RService.

---
+++ src/RService.IO/Abstractions/RoutingHttpContextExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using RService.IO.Router;
+
+namespace RService.IO.Abstractions
+{
+    public static class RoutingHttpContextExtensions
+    {
+        public static RequestDelegate GetRouteHandler(this HttpContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            var routingFeature = context.Features[typeof(IRoutingFeature)] as RoutingFeature;
+            return routingFeature?.RouteHandler;
+        }
+    }
+}
