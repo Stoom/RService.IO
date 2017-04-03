be1eadbb00801c4d16f519fcf12892d380c9552b
James Stumme <JamesStumme@Gmail.com>
2017-04-02 20:11:47 -0500

    Add sample projects.

---
+++ samples/RServiceSample/Models/GetItemValueReq.cs
@@ -0,0 +1,10 @@
+﻿using RService.IO.Abstractions;
+
+namespace RServiceSample.Models
+{
+    [Route("api/Values/{id}", RestVerbs.Get)]
+    public class GetItemValueReq
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ samples/RServiceSample/Models/UpdateItemValueReq.cs
@@ -0,0 +1,11 @@
+﻿using RService.IO.Abstractions;
+
+namespace RServiceSample.Models
+{
+    [Route("api/Values/{id}", RestVerbs.Put)]
+    public class UpdateItemValueReq
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+    }
+}
